266b8e758cdbffe9e57762c58632b10f2afdf486
vagrant <t.oleary@me.com>
2021-07-05 00:09:29 +0000

    while loop to repeat chorus

---
+++ chorus-while.js
@@ -0,0 +1,10 @@
+const chorus = "Let's dance!";
+let repeat = 0;
+while (repeat < 10) {
+  console.log(chorus);
+  if (repeat === 5) {
+  console.log("*change key*")
+  }
+  repeat++;
+}
+console.log("Until the sun comes up!");
