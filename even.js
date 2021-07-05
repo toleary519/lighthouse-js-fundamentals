1317ab7bef8e59cdc359fa7c0fc366cca69ffc5e
vagrant <t.oleary@me.com>
2021-07-05 22:07:17 +0000

    checking even numbers

---
+++ even.js
@@ -0,0 +1,12 @@
+let isEven  = function (num) {
+  return num % 2 === 0;
+}
+
+let tenIsEven = isEven(10);
+let elevenIsEven = isEven(11);
+
+//console.log(tenIsEven);
+//console.log(elevenIsEven);
+
+console.log(isEven(10));
+console.log(isEven(11));
