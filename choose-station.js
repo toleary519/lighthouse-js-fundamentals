d032c01c0ca99e96e1b527853039022930ca23f3
vagrant <t.oleary@me.com>
2021-07-06 00:44:47 +0000

    does not deep equal

--- choose-station.js
+++ choose-station.js
@@ -1,10 +1,30 @@
-let chooseStations = function (stations) {
-  let goodStations = ""
-  for (station of stations) {
-    if (station[2] >= 20 && station[3] === 'school' || 'community centre')
-    return station[1];
-    goodStations = station[1] += stations;
+let stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre'],
+  ['A', 10, 'school'],
+  ['B', 9, 'restaurant'],
+  ['C', 21, 'community centre'],
+  ['D', 15, 'school'],
+  ['E', 50, 'restaurant'],
+  ['F', 20, 'school'],
+  ['G', 40, 'community centre'],
+  ['H', 50, 'school']
+];
+
+function chooseStations(stations) {
+  const goodStations = [];
+
+  for (const station of stations) {
+    const capacity = station[1];
+    const type = station[2];
+    if ((capacity >= 20) && (type == 'school' || 'community centre')) {
+      goodStations.push(station[0]);
+    }  
   }
-return goodStations;
+  return goodStations;
 }
-console.log(chooseStations(stations));
+chooseStations(stations);
+
+
+
